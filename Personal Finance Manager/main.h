5662a7bcb125aa180bef02064cc5d855acf9cb43
Michał Witwicki <gaderivsgaming@gmail.com>
2023-11-29 15:44:06 +0100

    Budgeting
    Added option to plan budget

--- Personal Finance Manager/main.h
+++ Personal Finance Manager/main.h
@@ -13,3 +13,5 @@ void adding_income(double& income, std::string& incomeDescription, FinanceManage
 void adding_Expense(double& amount, FinanceManager& financeManager, int& categoryChoice, std::string& category, std::string& description, int& retFlag);
 
 void user_creation(User& user, FinanceManager& financeManager);
+
+void generate_budget_plan(int& month, int& year, double& salary);
